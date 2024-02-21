73f6cf4d24ddfbb0dba4ca0cd50fb91c3873c96c
Baarratia <baarratia@uc.cl>
2024-02-21 15:21:31 +0100

    Support for ESP32S3 (T3S3) implemented

--- PyLora_SX127x_extensions/pyLora.py
+++ PyLora_SX127x_extensions/pyLora.py
@@ -1,63 +1,58 @@
 import os
+import gc
 from PyLora_SX127x_extensions.constants import *
+
+# Attempt to get machine type
 try:
     machine = os.uname().machine
 except Exception:
     machine = os.name
-import gc
+
+# Enable garbage collection
 gc.enable()
 
 class pyLora:
     IS_RPi = machine.startswith('armv')
     IS_ESP8266 = machine.startswith('ESP8266')
-    IS_ESP32 = machine.startswith('ESP32')
+    IS_ESP32 = machine.startswith('ESP32') and not machine.startswith('Generic ESP32S3')
     IS_LORA32 = machine.startswith('LILYGO')
-    IS_LOPY = machine.startswith('LoPy')
+    IS_ESP32S3 = machine.startswith('Generic ESP32S3')  # Corrected check for ESP32S3
 
     __SX127X_LIB = None
 
     timeout_socket = None
     blocked_socket = None
 
-    def __init__(self, verbose=False,
-                 do_calibration=False,
-                 calibration_freq=868,
-                 sf=7,
-                 cr=8,
-                 freq=868):
-
+    def __init__(self, verbose=False, do_calibration=False, calibration_freq=868, sf=7, cr=8, freq=868):
         auto_board_selection = None
 
         if self.IS_RPi:
             from PyLora_SX127x_extensions.board_config_rpi import BOARD_RPI
             auto_board_selection = BOARD_RPI
 
-        if self.IS_ESP32 or self.IS_LORA32:
+        elif self.IS_ESP32 or self.IS_LORA32:
             from PyLora_SX127x_extensions.board_config_esp32 import BOARD_ESP32
             auto_board_selection = BOARD_ESP32
 
-        if self.IS_LOPY:
-            from network import LoRa
-            import socket
+        elif self.IS_ESP32S3:
+            # Import and use the BOARD_ESP32S3 class
+            from PyLora_SX127x_extensions.board_config_esp32s3 import BOARD_ESP32S3
+            auto_board_selection = BOARD_ESP32S3
 
-            loraLoPyModule = LoRa(mode=LoRa.LORA, region=LoRa.EU868, frequency=freq*1000000, coding_rate=LoRa.CODING_4_5,
-                        bandwidth=LoRa.BW_125KHZ, sf=sf)
-            self.lopyLora = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-        else:
-            # Collect garbage and check the amount of memory allocated and free again
-            gc.collect()
-            from PyLora_SX127x_extensions.LoRa import LoRa
-            self.__SX127X_LIB = LoRa(Board_specification=auto_board_selection,
-                                     verbose=verbose,
-                                     do_calibration=do_calibration,
-                                     calibration_freq=calibration_freq,
-                                     cr=CODING_RATE.CR4_5,
-                                     sf=sf,
-                                     freq=freq,
-                                     rx_crc=True)
+        # Collect garbage and check the amount of memory allocated and free again
+        gc.collect()
+        from PyLora_SX127x_extensions.LoRa import LoRa
+        self.__SX127X_LIB = LoRa(Board_specification=auto_board_selection,
+                                 verbose=verbose,
+                                 do_calibration=do_calibration,
+                                 calibration_freq=calibration_freq,
+                                 cr=CODING_RATE.CR4_5,
+                                 sf=sf,
+                                 freq=freq,
+                                 rx_crc=True)
+
 
     def send(self, content):
-        if self.IS_LOPY: return self.lopyLora.send(content)
         self.__SX127X_LIB.set_mode(MODE.SLEEP)
         self.__SX127X_LIB.set_dio_mapping([1, 0, 0, 0])  # DIO0 = 1 is for TXDone, transmitting mode basically
         self.__SX127X_LIB.set_mode(MODE.STDBY)
@@ -70,7 +65,6 @@ class pyLora:
         """ Util Method for recv
             It will turn automatically the device on receive mode
         """
-        if self.IS_LOPY: return self.lopyLora.recv(size)
         self.__SX127X_LIB.set_mode(MODE.SLEEP)
         self.__SX127X_LIB.set_dio_mapping([0, 0, 0, 0])
         self.__SX127X_LIB.set_mode(MODE.RXCONT)
@@ -81,16 +75,13 @@ class pyLora:
         """ set timeout for operations
             After we determine if we want to send or receive, we need to specify a timeout
         """
-        if self.IS_LOPY: return self.lopyLora.settimeout(value)
         self.timeout_socket = value
 
 
     def setblocking(self, value):
-        if self.IS_LOPY: return self.lopyLora.setblocking(value)
         self.blocked_socket = value
 
     def get_rssi(self):
-        if self.IS_LOPY: return self.lopyLora.stats()[1]
         return self.__SX127X_LIB.get_pkt_rssi_value()
 
     def sf(self, sf):
